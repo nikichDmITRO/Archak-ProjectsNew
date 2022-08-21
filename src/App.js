14374a43d7ff3b6e3d3218f2b90908f87888a99f
nikichDmITRO <dmitracenkonikita5@gmail.com>
2022-08-21 10:42:13 +0300

    счетчик

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './index.scss';
 
 function App() {
+const[count,setCount]=useState(0)
+const setCountPlus=()=>{
+  setCount(count+1)
+}
+const setCountMinus=()=>{
+  setCount(count-1)
+}
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
         <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <button onClick={setCountMinus} className="minus">- Минус</button>
+        <button  onClick={setCountPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
